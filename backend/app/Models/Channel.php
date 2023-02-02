d7b2035e8c2757bd47cea07aee2d7bd5a7116101
seidaiegoshi <egoshi.seidai@gmail.com>
2023-02-02 19:06:09 +0900

    作ったchannelsテーブルようのModelを変更していく

--- backend/app/Models/Channel.php
+++ backend/app/Models/Channel.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Channel extends Model
 {
     use HasFactory;
+
+    // 実際のテーブルが、クラス名の複数形＋スネークケースであれば、書かなくてもOK
+    protected $table = 'channels';
+
+    // Eloquentを通して更新や登録が可能なフィールド（ホワイトリストを定義）
+    protected $fillable = ['uuid', 'name'];
 }

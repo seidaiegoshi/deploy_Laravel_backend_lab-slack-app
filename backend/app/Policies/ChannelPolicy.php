a573fc87afde4efb64fd54e3d8156eda1a554613
seidaiegoshi <egoshi.seidai@gmail.com>
2023-02-24 18:18:23 +0900

    チャンネルポリシーの説明

--- backend/app/Policies/ChannelPolicy.php
+++ backend/app/Policies/ChannelPolicy.php
@@ -13,6 +13,8 @@ class ChannelPolicy
 
     public function show(User $user, Channel $channel)
     {
+        //渡されたチャンネルのユーザーに自分のユーザーidが含まれているかどうか。
+        //trueだとみれる、falseだと見れない。
         return $channel->users->contains('id', $user->id);
     }
 }
